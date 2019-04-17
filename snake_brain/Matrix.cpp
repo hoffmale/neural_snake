5078e271117c5a0a03f73e6cd248a104e4583749
wwolfgang11 <morgan.weiss@outlook.com>
2019-04-16 21:18:40 -0700

    Matrix implementation, not fully tested or finished. Needs Lennart's attention.

--- snake_brain/Matrix.cpp
+++ snake_brain/Matrix.cpp
@@ -15,16 +15,16 @@ Matrix::Matrix(std::vector<std::vector<double>> const& matrix) :
 	num_cols(matrix[0].size()),
 	matrix(matrix) {}
 
-Matrix::Matrix(Matrix const& source)
-{
-	for (int i = 0; i < source.num_rows; ++i)
-	{
-		for (int j = 0; j < source.num_cols; ++j)
-		{
-
-		}
-	}
-}
+//Matrix::Matrix(Matrix const& source)
+//{
+//	for (int i = 0; i < source.num_rows; ++i)
+//	{
+//		for (int j = 0; j < source.num_cols; ++j)
+//		{
+//			
+//		}
+//	}
+//}
 
 Matrix Matrix::multiply(double const& c) const
 {
@@ -38,3 +38,150 @@ Matrix Matrix::multiply(double const& c) const
 	}
 	return result;
 }
+
+Matrix Matrix::add(Matrix const& matrix1, Matrix const& matrix2) const
+{
+	assert(num_rows == matrix1.num_rows && num_cols == matrix1.num_cols &&
+		num_rows == matrix2.num_rows && num_cols == matrix2.num_cols);
+
+	Matrix result(matrix1.num_rows, matrix1.num_cols);
+	for (int i = 0; i < result.num_rows; ++i)
+	{
+		for (int j = 0; j < result.num_cols; ++j)
+		{
+			result.matrix[i][j] = matrix1.matrix[i][j] + matrix2.matrix[i][j];
+		}
+	}
+	return result;
+}
+
+Matrix Matrix::subtract(Matrix const& matrix1, Matrix const& matrix2) const
+{
+	assert(num_rows == matrix1.num_rows && num_cols == matrix1.num_cols &&
+		num_rows == matrix2.num_rows && num_cols == matrix2.num_cols);
+
+	Matrix result(matrix1.num_rows, matrix1.num_cols);
+	for (int i = 0; i < result.num_rows; ++i)
+	{
+		for (int j = 0; j < result.num_cols; ++j)
+		{
+			result.matrix[i][j] = matrix1.matrix[i][j] - matrix2.matrix[i][j];
+		}
+	}
+	return result;
+}
+
+Matrix Matrix::multiply(Matrix const& matrix1, Matrix const& matrix2) const
+{
+	assert(num_rows == matrix1.num_rows && num_cols == matrix1.num_cols &&
+		num_rows == matrix2.num_rows && num_cols == matrix2.num_cols);
+
+	Matrix result(matrix2.num_rows, matrix2.num_cols);
+	for (int i = 0; i < result.num_rows; ++i)
+	{
+		for (int j = 0; j < result.num_cols; ++j)
+		{
+			result.matrix[i][j] = matrix1.matrix[i][j] * matrix2.matrix[i][j];
+		}
+	}
+	return result;
+}
+
+Matrix Matrix::dot(Matrix const& matrix1) const
+{
+	assert(num_rows == matrix1.num_cols);
+	int m_cols = matrix1.num_cols;
+	double w = 0;
+	Matrix result(matrix1.num_rows, m_cols);
+	for (int i = 0; i < result.num_rows; ++i)
+	{
+		for (int j = 0; j < m_cols; ++j)
+		{
+			for (int k = 0; k < result.num_cols; ++k)
+			{
+				w += matrix[i][k] * matrix1.matrix[k][j];
+			}
+			result.matrix[i][j] = w;
+		}
+	}
+	return result;
+}
+
+Matrix Matrix::transpose() const
+{
+	Matrix result(num_rows, num_cols);
+	for (int i = 0; i < num_rows; ++i)
+	{
+		for (int j = 0; j < num_cols; ++j)
+		{
+			result.matrix[i][j] = matrix[j][i];
+		}
+	}
+	return result;
+}
+
+Matrix Matrix::normalize(Matrix const& matrix1)
+{
+	Matrix result(matrix1.num_rows, matrix1.num_cols);
+	for (int i = 0; i < result.num_rows; ++i)
+	{
+		double sum = 0;
+		for (int j = 0; j < result.num_cols; ++j)
+		{
+			sum += matrix1.matrix[i][j];
+		}
+		for (int k = 0; k < num_cols; ++k)
+		{
+			result.matrix[i][k] = matrix1.matrix[i][k] / sum;
+		}
+	}
+	return result;
+}
+
+Matrix Matrix::flatten(Matrix const& matrix1)
+{
+	Matrix result(matrix1.num_rows*matrix1.num_cols, 1);
+	for (int i = 0; i < result.num_rows; ++i)
+	{
+		for (int j = 0; j < result.num_cols; ++j)
+		{
+
+		}
+	}
+	return result;
+}
+
+Matrix Matrix::apply_function(double(*function)(double)) const
+{
+	Matrix result(num_rows, num_cols);
+	for (int i = 0; i < num_rows; ++i)
+	{
+		for (int j = 0; j < num_cols; ++j)
+		{
+			result.matrix[i][j] = (*function)(matrix[i][j]);
+		}
+	}
+	return result;
+}
+
+void Matrix::fill()
+{
+	Matrix result(num_rows, num_cols);
+	for (int i = 0; i < num_rows; ++i)
+	{
+		for (int j = 0; j < num_cols; ++j)
+		{
+			result.matrix[i][j] = 1 + i;
+		}
+	}
+}
+
+void Matrix::print() const {
+	for (unsigned i = 0; i < matrix.size(); i++) {
+		for (unsigned j = 0; j < matrix[i].size(); j++) {
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
--- snake_brain/Matrix.h
+++ snake_brain/Matrix.h
@@ -9,21 +9,24 @@ public:
 	Matrix() {}																// Empty constructor		
 	Matrix(int num_rows, int num_cols);										// Constructor	
 	Matrix(std::vector<std::vector<double>> const& mat);
-	Matrix(Matrix const& source);										    // Copy constructor
+	//Matrix(Matrix const& source);										    // Copy constructor
 
 
-	
+
 	Matrix multiply(double const& c) const;									// Scalar multiplication
-	Matrix subtract(Matrix const& matrix1, Matrix const& matrix2) const;	// Subtraction of matrices
+	Matrix add(Matrix const& matrix1, Matrix const& matrix2) const;			// Adding of matrices
+	Matrix subtract(Matrix const& matrix1, Matrix const& matrix2) const;	// Subtraction of matrix1 - matrix2
 	Matrix multiply(Matrix const& matrix1, Matrix const& matrix2) const;	// Hadamard product (entrywise product)
-	Matrix dot(Matrix const& matrix1, Matrix const& matrix2) const;		    // Dot product of 2 matrices
+	Matrix dot(Matrix const& matrix) const;								    // Dot product of 2 matrices
 	Matrix transpose() const;												// Transpose matrix
 	Matrix normalize(Matrix const& matrix1);								// Normalize a matrix such that the sum of each row is 1
 	Matrix flatten(Matrix const& matrix1);									// Convert 2D matrix to 1D vector
 
 	Matrix apply_function(double(*function)(double)) const;					// Used to apply a function to the entire matrix
 
-	void print(std::ostream& flux);											// Print matrix
+	void print() const;														// Print matrix
+
+	void fill();
 
 private:
 	std::vector<std::vector<double>> matrix;
@@ -32,5 +35,3 @@ private:
 
 };
 
-// Overload << operator to print
-std::ostream& operator<<(std::ostream& flux, Matrix const& mat);
